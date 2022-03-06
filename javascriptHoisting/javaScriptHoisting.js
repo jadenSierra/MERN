6b0a801c707439a4184982aa6bdb484e3a0493dc
Jaden Sierra <JadenSierra31@gmail.com>
2022-03-05 17:28:19 -0800

    initial_commit

--- javascriptHoisting/javaScriptHoisting.js
+++ javascriptHoisting/javaScriptHoisting.js
@@ -73,21 +73,3 @@ function learn() {
 
 learn();
 console.log(dojo);
-
-//8
-
-
-function makeDojo(name, students){
-    const dojo = {};
-    dojo.name = name;
-    dojo.students = students;
-    if(dojo.students > 50){
-        dojo.hiring = true;
-    }
-    else if(dojo.students <= 0){
-        dojo.name = "closed for now";
-    }
-    return dojo;
-}
-console.log(makeDojo("Chicago", 65));
-console.log(makeDojo("Berkeley", 0));
